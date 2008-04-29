d3ccd92360a0d88bc166f168602a4a1e7047f5d0
domkon <domkon@c710177b-734a-0410-8252-b3c0eaf472a8>
2008-04-29 21:33:47 +0000

    AuffuehrungsHelper.java ersten beiden Fktn implementiert und rudimentär getestet, Stundenliste/Dominik Kontner.xls update

--- trunk/Implementierung/ticketline/src/java/ticketline/helper/AuffuehrungsHelper.java
+++ trunk/Implementierung/ticketline/src/java/ticketline/helper/AuffuehrungsHelper.java
@@ -9,6 +9,11 @@ import java.util.Date;
 import java.util.List;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import ticketline.dao.DAOFactory;
+import ticketline.dao.hibernate.AuffuehrungDAOHibernate;
+import ticketline.dao.interfaces.AuffuehrungDAO;
+import ticketline.dao.interfaces.KuenstlerDAO;
+import ticketline.dao.interfaces.VeranstaltungDAO;
 import ticketline.db.Auffuehrung;
 import ticketline.db.Kuenstler;
 import ticketline.db.SaalKey;
@@ -19,7 +24,7 @@ import ticketline.exceptions.TicketLineSystemException;
 
 /**
  *
- * @author Michael Morak
+ * @author Dominik Kontner
  */
 public class AuffuehrungsHelper 
 {
@@ -27,20 +32,56 @@ public class AuffuehrungsHelper
     
     private AuffuehrungsHelper() { }
     
-    public static List<Kuenstler> sucheKuenstler(String vorname, String nachname, boolean maennlich) throws TicketLineException, TicketLineSystemException
+    public static List<Kuenstler> sucheKuenstler(String vorname, String nachname, boolean maennlich) 
+            throws TicketLineException, TicketLineSystemException
     {
-        return null;
+        KuenstlerDAO kuenstler = DAOFactory.getKuenstlerDAO();
+        
+        String query = "1 = 1 ";
+        
+        if (vorname != null) query += "AND vname = '" + vorname + "'";
+        if (nachname != null) query += "AND nname = '" + nachname + "'";
+        if (maennlich != false) query += "AND geschlecht = '" + maennlich + "'";
+    
+        List list = kuenstler.find(query);
+        log.info(list);
+        
+        return list;
     }
     
     public static List<Veranstaltung> sucheVeranstaltungen(String bezeichnung, String kategorie,
-                                                    Integer dauerMin, Integer dauerMax, String inhalt) throws TicketLineException, TicketLineSystemException
+                                                    Integer dauerMin, Integer dauerMax, String inhalt) 
+                                                    throws TicketLineException, TicketLineSystemException
     {
-        return null;
+         VeranstaltungDAO veranstaltung = DAOFactory.getVeranstaltungDAO();
+        
+        String query = "1=1";
+        
+        if (bezeichnung != null) query += "AND bezeichnung = '" + bezeichnung + "'";
+        if (kategorie != null) query += "AND kategorie = '" + kategorie + "'";
+        if (inhalt != null) query += "AND inhalt = '" + inhalt + "'";
+        if (dauerMin != null && dauerMax != null) query += "AND dauer BETWEEN '" + dauerMin + "' AND '" + dauerMax + "'";
+         
+        List list = veranstaltung.find(query);
+        
+        log.info(list);
+        return list;
     }
     
     public static List<Auffuehrung> sucheAuffuehrungen(Date zeitVon, Date zeitBis, boolean storniert, Integer preisMin, Integer preisMax,
                                                 VeranstaltungKey veranstaltung, SaalKey saal) throws TicketLineException, TicketLineSystemException
     {
+        AuffuehrungDAO auffuehrung = DAOFactory.getAuffuehrungDAO();
+        
+       
+        
+       // List list = auffuehrung.find("date BETWEEN '" + zeitVon + "' AND '" + zeitBis + "' AND storniert = " + storniert + "' AND preis BETWEEN '"
+       //         + preisMin + "' AND '" + preisMax + "' AND ");
+        
+      //  alter table auffuehrung add constraint FK66B638A019C5BD90 foreign key (saalbez, ortbez, ort) references saal;
+      //  alter table auffuehrung add constraint FK66B638A099F20D47 foreign key (veranstbez, veranstkat) references veranstaltung;
+        
+       // log.info(list);
         return null;
     }
 }
