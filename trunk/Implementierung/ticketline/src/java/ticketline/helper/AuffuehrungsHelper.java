baa59cb2d50651a2f49a58f074cf5bfd69c34e94
domkon <domkon@c710177b-734a-0410-8252-b3c0eaf472a8>
2008-04-30 10:45:31 +0000

    klasse fertig implementiert, bei meinem test ging die sucheauffühung nicht wirklich, hab aber den fehler noch nicht gefunden, weil ich keine fehlermeldungen bekomme, er tut einfach garnix ... ganz unten als kommentar meine testfälle, die in die MainPage.java in die init() zum einfügen wären - zum testen

--- trunk/Implementierung/ticketline/src/java/ticketline/helper/AuffuehrungsHelper.java
+++ trunk/Implementierung/ticketline/src/java/ticketline/helper/AuffuehrungsHelper.java
@@ -6,6 +6,7 @@
 package ticketline.helper;
 
 import java.util.Date;
+
 import java.util.List;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -32,18 +33,20 @@ public class AuffuehrungsHelper
     
     private AuffuehrungsHelper() { }
     
-    public static List<Kuenstler> sucheKuenstler(String vorname, String nachname, boolean maennlich) 
+    public static List<Kuenstler> sucheKuenstler(String vorname, String nachname, Boolean maennlich) 
             throws TicketLineException, TicketLineSystemException
     {
         KuenstlerDAO kuenstler = DAOFactory.getKuenstlerDAO();
         
         String query = "1 = 1 ";
         
-        if (vorname != null) query += "AND vname = '" + vorname + "'";
-        if (nachname != null) query += "AND nname = '" + nachname + "'";
-        if (maennlich != false) query += "AND geschlecht = '" + maennlich + "'";
+        if (vorname != null) query +=           "AND vname like '%" + vorname + "%' ";
+        if (nachname != null) query +=          "AND nname like '%" + nachname + "%' ";
+        if (maennlich != null) query +=         "AND geschlecht = '" + (maennlich ? "M" : "W") + "' ";
     
         List list = kuenstler.find(query);
+        
+        log.info("KÜNSTLER" + query); 
         log.info(list);
         
         return list;
@@ -53,35 +56,99 @@ public class AuffuehrungsHelper
                                                     Integer dauerMin, Integer dauerMax, String inhalt) 
                                                     throws TicketLineException, TicketLineSystemException
     {
-         VeranstaltungDAO veranstaltung = DAOFactory.getVeranstaltungDAO();
+        VeranstaltungDAO veranstaltung = DAOFactory.getVeranstaltungDAO();
         
-        String query = "1=1";
+        String query = "1=1 ";
         
-        if (bezeichnung != null) query += "AND bezeichnung = '" + bezeichnung + "'";
-        if (kategorie != null) query += "AND kategorie = '" + kategorie + "'";
-        if (inhalt != null) query += "AND inhalt = '" + inhalt + "'";
-        if (dauerMin != null && dauerMax != null) query += "AND dauer BETWEEN '" + dauerMin + "' AND '" + dauerMax + "'";
+        if (bezeichnung != null) query +=                   "OR bezeichnung like '%" + bezeichnung + "%' ";
+        if (kategorie != null) query +=                     "OR kategorie like '%" + kategorie + "%' ";
+        if (inhalt != null) query +=                        "OR inhalt like '%" + inhalt + "%' ";
+        if (dauerMin != null && dauerMax != null) query +=  "AND dauer BETWEEN '" + dauerMin + "' AND '" + dauerMax + "' ";
          
         List list = veranstaltung.find(query);
         
+        log.info("das andere" + query); 
         log.info(list);
         return list;
     }
     
-    public static List<Auffuehrung> sucheAuffuehrungen(Date zeitVon, Date zeitBis, boolean storniert, Integer preisMin, Integer preisMax,
+    public static List<Auffuehrung> sucheAuffuehrungen(Date zeitVon, Date zeitBis, Boolean storniert, Integer preisMin, Integer preisMax,
                                                 VeranstaltungKey veranstaltung, SaalKey saal) throws TicketLineException, TicketLineSystemException
     {
-        AuffuehrungDAO auffuehrung = DAOFactory.getAuffuehrungDAO();
-        
        
+       String veranstaltungBezeichnung  = veranstaltung.getBezeichnung();
+       String veranstaltungKategorie = veranstaltung.getKategorie() ;
         
-       // List list = auffuehrung.find("date BETWEEN '" + zeitVon + "' AND '" + zeitBis + "' AND storniert = " + storniert + "' AND preis BETWEEN '"
-       //         + preisMin + "' AND '" + preisMax + "' AND ");
+       String saalBezeichnung =             null;
+       String saalOrt =                     null;
+       String saalOrtBezeichnung =          null;
+       
+       java.sql.Date sqlZeitVon = null;
+       java.sql.Date sqlZeitBis = null;
         
-      //  alter table auffuehrung add constraint FK66B638A019C5BD90 foreign key (saalbez, ortbez, ort) references saal;
-      //  alter table auffuehrung add constraint FK66B638A099F20D47 foreign key (veranstbez, veranstkat) references veranstaltung;
-        
-       // log.info(list);
-        return null;
+       AuffuehrungDAO auffuehrung = DAOFactory.getAuffuehrungDAO();
+       
+       String query = " 1=1 ";
+       log.error("trööööt");
+       if (veranstaltung != null) {
+           
+           veranstaltungBezeichnung  = veranstaltung.getBezeichnung();
+           veranstaltungKategorie = veranstaltung.getKategorie() ;
+       }
+       
+       if (saal != null){
+       saalBezeichnung =             saal.getBezeichnung();     
+       saalOrt =                     saal.getOrt();
+       saalOrtBezeichnung =          saal.getOrtbez();
+       }
+       
+       if (zeitVon != null && zeitBis != null){
+       sqlZeitVon = java.sql.Date.valueOf(zeitVon.toString());
+       sqlZeitBis = java.sql.Date.valueOf(zeitBis.toString());
+       }
+       
+       log.info(sqlZeitVon.toString());
+       
+       if (zeitVon != null && zeitBis != null) query +=         "AND datumuhrzeit BETWEEN '" + sqlZeitVon.toString() + "' AND '" + sqlZeitBis.toString() + "' ";
+       if (storniert != null) query +=                          "AND storniert = '" + storniert + "' ";
+       if (preisMin != null && preisMax != null) query +=       "AND preis BETWEEN '" + preisMin + "' AND '" + preisMax + "' ";
+       
+       if (veranstaltungBezeichnung != null) query +=           "OR bezeichnung like %'" + veranstaltungBezeichnung + "'% ";
+       if (veranstaltungKategorie != null) query +=             "OR kategorie like %'" + veranstaltungKategorie + "'% ";
+       if (saalBezeichnung != null) query +=                    "OR bezeichnung like %'" + saalBezeichnung + "'% ";
+       if (saalOrtBezeichnung != null) query +=                 "OR ortbez like %'" + saalOrtBezeichnung + "'% ";
+       if (saalOrt != null) query +=                            "OR ort like %'" + saalOrt + "'% ";
+       
+       
+       List list = auffuehrung.find(query);
+       
+       log.info("das ganz ganz ganz ganz andere " + query); 
+       log.info(list);
+       return list;
     }
 }
+/*
+ try{
+        ticketline.helper.AuffuehrungsHelper.sucheKuenstler("e", "e", false);
+        }catch(Exception ex){
+            
+        }
+
+        try{
+         ticketline.helper.AuffuehrungsHelper.sucheVeranstaltungen("ee", "e", 0, 200, "");
+        }catch(Exception ex){
+            
+        }
+        
+       
+        try{
+            
+        //AuffuehrungsHelper.sucheAuffuehrungen(zeitVon, zeitBis, storniert, preisMin, preisMax, veranstaltung, saal);
+        ticketline.helper.AuffuehrungsHelper.sucheAuffuehrungen(null, null, false, 0, 300, null, null);
+        
+        
+        }catch(Exception ex){
+            
+        }
+
+*/
