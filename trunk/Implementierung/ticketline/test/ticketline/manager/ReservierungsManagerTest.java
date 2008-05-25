4f0422fd2670cbec6d9c021ade931d0d9d189007
rensab <rensab@c710177b-734a-0410-8252-b3c0eaf472a8>
2008-05-25 11:53:08 +0000

    Testgegenstände, Iterationsplan eingetragen, ReservierungsManagerTest erstellt

--- trunk/Implementierung/ticketline/test/ticketline/manager/ReservierungsManagerTest.java
+++ trunk/Implementierung/ticketline/test/ticketline/manager/ReservierungsManagerTest.java
@@ -14,12 +14,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import ticketline.dao.DAOFactory;
+import ticketline.dao.interfaces.BestellungDAO;
 import ticketline.dao.interfaces.KundeDAO;
 import ticketline.dao.interfaces.TransaktionDAO;
 import ticketline.db.Auffuehrung;
 import static org.junit.Assert.*;
 import ticketline.db.AuffuehrungKey;
 import ticketline.db.Belegung;
+import ticketline.db.Bestellung;
 import ticketline.db.Kategorie;
 import ticketline.db.Kunde;
 import ticketline.db.Reihe;
@@ -115,16 +117,60 @@ public class ReservierungsManagerTest {
         
     }
 
-    /**
-     * Test of storniereReservierung method, of class ReservierungsManager.
-     *
     @Test
-    public void storniereReservierung() throws Exception {
-        System.out.println("storniereReservierung");
-        TransaktionKey reservierung = null;
-        ReservierungsManager.storniereReservierung(reservierung);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void reserviereTicket2() throws Exception {
+        System.out.println("reserviere und storniere Ticket");
+        TransaktionDAO transaktionDAO = DAOFactory.getTransaktionDAO();
+        int anzTransStart = transaktionDAO.find("storniert is 'false'").size();
+        KundeDAO kundeDAO = DAOFactory.getKundeDAO();
+        Kunde kunde = kundeDAO.get(1);
+        Date zeit = new Date();
+        Auffuehrung auffuehrung = (Auffuehrung) DAOFactory.getAuffuehrungDAO().getAll().get(2);
+        AuffuehrungKey auffuehrungsKey = auffuehrung.getComp_id();
+        ReiheKey reihe = ((Reihe) ((Kategorie) auffuehrung.getSaal().getKategorien().iterator().next()).getReihen().iterator().next()).getComp_id();
+        BigDecimal preis = new BigDecimal(100);
+        Integer startplatz = 1;
+        Integer anzahl = 4;
+        String zahlart = "Karte";
+        boolean reservierung = true;
+        Integer expResult = new Integer(-1);
+        Integer result = ReservierungsManager.kaufeTickets(kunde, zeit, auffuehrungsKey, reihe, preis, startplatz, anzahl, zahlart, reservierung);
+        System.out.println("Die reservierungsnummer lautet: " + result);
+        int anzTransRes = transaktionDAO.find("storniert is 'false'").size();
+        assertFalse(expResult == result);
+        assertTrue(anzTransStart+1 == anzTransRes);
+        System.out.println(anzTransStart+1 + " zu "+ anzTransRes);
+        TransaktionKey transaktionKey = ((Transaktion)DAOFactory.getTransaktionDAO().find("resnr is " + result).get(0)).getComp_id();
+        ReservierungsManager.storniereReservierung(transaktionKey);
+        int anzTransStor = transaktionDAO.find("storniert is 'false'").size();
+        System.out.println(anzTransStart + " zu "+ anzTransStor);
+        assertTrue(anzTransStart==anzTransStor);
+        
+        System.out.println("Ende gut, alles gut");
+        
     }
-    **/
+    
+       @Test
+    public void kaufeWerbematerial() throws Exception {
+        System.out.println("Kaufe Werbematerial");
+        KundeDAO kundeDAO = DAOFactory.getKundeDAO();
+        Kunde kunde = kundeDAO.get(1);
+        BestellungDAO bestellungDAO = DAOFactory.getBestellungDAO();
+        Bestellung bestellung = (Bestellung) bestellungDAO.getAll().get(0);
+        ReservierungsManager.kaufeWerbematerial(kunde, bestellung);
+        assertEquals(bestellung.getKunde(),kunde);
+    }
+       
+         @Test
+    public void kaufeWerbematerial2() throws Exception {
+        System.out.println("Kaufe Werbematerial");
+        KundeDAO kundeDAO = DAOFactory.getKundeDAO();
+        Kunde kunde = kundeDAO.get(1);
+        BestellungDAO bestellungDAO = DAOFactory.getBestellungDAO();
+        Bestellung bestellung = (Bestellung) bestellungDAO.getAll().get(4);
+        ReservierungsManager.kaufeWerbematerial(kunde, bestellung);
+        assertEquals(bestellung.getKunde(),kunde);
+    }
+    
+    
 }
